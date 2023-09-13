34cda3d8b7325fc8ae3fce1be7b988278511d191
lucap-insight <115674330+lucap-insight@users.noreply.github.com>
2023-09-13 16:14:49 +1000

    Updated ConnectWiseTicket
    Added instance variable config in constructor.
    Commented out static ConnectWiseAPICall and created new ConnectWiseAPICall that makes use of config.

--- src/main/java/com/insightsystems/symphony/tal/ConnectWiseTicket.java
+++ src/main/java/com/insightsystems/symphony/tal/ConnectWiseTicket.java
@@ -1,6 +1,7 @@
 package com.insightsystems.symphony.tal;
 
 import com.avispl.symphony.api.tal.dto.TicketSourceConfigProperty;
+import com.avispl.symphony.api.tal.dto.TicketSystemConfig;
 import com.avispl.symphony.api.tal.error.TalAdapterSyncException;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,6 +34,12 @@ public class ConnectWiseTicket {
      * Logger instance
      */
     private static final Logger logger = LoggerFactory.getLogger(ConnectWiseTicket.class);
+
+    /**
+     * Instance of TicketSystemConfig that contains mappings and destination
+     * ticketing system configuration
+     */
+    private TicketSystemConfig config;
 
     /**
      * Symphony unique ticket ID
@@ -91,15 +98,120 @@ public class ConnectWiseTicket {
     //---------------------------------//
     //* ---------- METHODS ---------- *//
     //---------------------------------//
-    public ConnectWiseTicket(String symphonyId, String symphonyLink, String id, String url) {
+    public ConnectWiseTicket(TicketSystemConfig config, String symphonyId, String symphonyLink, String id, String url) {
         // logger.info("Initializing ConnectWise ticket")
+        setConfig(config);
         setSymphonyId(symphonyId);
         setSymphonyLink(symphonyLink);
         setId(id);
         setUrl(url);
     }
 
+    // FIXME: See if this is possible and/or needed
+    /*
     static public JSONObject ConnectWiseAPICall(String url, String method, String requestBody) throws TalAdapterSyncException {
+        // Optional: Formalize input error checking on ConnectWiseAPICall
+
+        // FIXME: What do I do with this? Where should I get this information from? How does CWTicket makes API calls?
+        String clientID = config.getTicketSourceConfig().get(TicketSourceConfigProperty.LOGIN);
+        String authorization = config.getTicketSourceConfig().get(TicketSourceConfigProperty.PASSWORD);
+
+        if (clientID == null || authorization == null) {
+            logger.error("ConnectWiseAPICall: Unable to retrieve client ID and/or authorization from configuration");
+            throw new NullPointerException("Error retrieving client ID and/or authorization. Null value encountered");
+        }
+
+        if (url == null) {
+            logger.error("ConnectWiseAPICall: URL cannot be null");
+            throw new NullPointerException("URL for API call cannot be null");
+        }
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = null;
+
+        try {
+            if (requestBody != null) {
+                request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .method(method, HttpRequest.BodyPublishers.ofString(requestBody))
+                        .header("Content-Type", "application/json")
+                        .header("clientID", clientID)
+                        .header("Authorization", authorization)
+                        .build();
+            } else if (Objects.equals(method, "GET")) {
+                request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .header("Content-Type", "application/json")
+                        .header("clientID", clientID)
+                        .header("Authorization", authorization)
+                        .build();
+            }
+        } catch (Exception e) {
+            logger.error("ConnectWiseAPICall: Error building HttpRequest: " + e.getMessage());
+            throw new TalAdapterSyncException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        // Response
+        HttpResponse<String> response = null;
+        logger.info("ConnectWiseAPICall: Getting response");
+        try {
+            // Send HTTP request
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            logger.error("ConnectWiseAPICall: HTTP request generated error: " + e.getMessage());
+            if (response != null) {
+                throw new TalAdapterSyncException(e + " - HTTP request error",
+                        HttpStatus.valueOf(response.statusCode()));
+            } else // Not recoverable. Without a response we can't be sure sending another request will fix it
+                throw new TalAdapterSyncException(e + " - HTTP request error");
+        }
+
+        if (response != null && (response.statusCode() == 200 || response.statusCode() == 201)) {
+            logger.info("ConnectWiseAPICall: {} call successful - HTTP Code: {}",
+                    method,
+                    response.statusCode());
+        } else {
+            // Log error
+            logger.error("ConnectWiseAPICall: {} call failed - HTTP Code: {}",
+                    method,
+                    response != null ? response.statusCode() : "not specified");
+
+            if (response != null && response.statusCode() == 408) { // Re-try time-out calls
+                // This will turn into a recoverable exception
+                throw new TalAdapterSyncException("ConnectWiseAPICall: request timed-out", HttpStatus.REQUEST_TIMEOUT);
+            }
+            throw new TalAdapterSyncException(method + " Request error",
+                    response != null ? HttpStatus.valueOf(response.statusCode()) : null);
+        }
+
+        JSONObject jsonObject;
+        try {
+            //System.out.println(response.body());
+            jsonObject = new JSONObject(response.body());
+        } catch (JSONException e) {
+            try {
+                // It is possible that the response is a JSON array, so it is put in a JSON object under JSONArray
+                jsonObject = new JSONObject("{ \"JSONArray\" : " + response.body() + "}");
+            } catch (JSONException e2) {
+                // If it is also not an Array: give up and report error
+                logger.error("ConnectWiseAPICall: error parsing content to JSON - " + e2);
+                logger.error("ConnectWiseAPICall: API call object: " + response.request());
+                return null;
+            }
+        }
+        return jsonObject;
+    }
+    */
+
+    /**
+     * Performs an HTTP request call to ConnectWise API using credentials set in config
+     * @param url the HTTP request URI
+     * @param method the HTTP method (i.e. GET)
+     * @param requestBody the HTTP request's body
+     * @return JSON object with the HTTP request response
+     * @throws TalAdapterSyncException if request fails
+     */
+    public JSONObject ConnectWiseAPICall(String url, String method, String requestBody) throws TalAdapterSyncException {
         // Optional: Formalize input error checking on ConnectWiseAPICall
 
         String clientID = config.getTicketSourceConfig().get(TicketSourceConfigProperty.LOGIN);
@@ -191,6 +303,7 @@ public class ConnectWiseTicket {
         return jsonObject;
     }
 
+
     /**
      * Creates and returns a new ConnectWiseTicket instance. Does not alter the current instance.
      *
@@ -199,11 +312,21 @@ public class ConnectWiseTicket {
     public ConnectWiseTicket sync() {
         // Instantiate synced ticket
         ConnectWiseTicket syncedCWTicket = new ConnectWiseTicket(
-                this.getSymphonyId(), this.getSymphonyLink(), this.getId(), this.getUrl());
+                getConfig(), getSymphonyId(), getSymphonyLink(), getId(), getUrl());
 
-
+        // TODO: Sync ticket bruh
 
         return syncedCWTicket;
+    }
+
+    /**
+     * Updates this ticket based on CWTicket.
+     * Makes changes to CWTicket as necessary.
+     *
+     * @param CWTicket
+     */
+    public void update(ConnectWiseTicket CWTicket) {
+
     }
 
     // Update comments
@@ -326,5 +449,11 @@ public class ConnectWiseTicket {
         Comments.add(CWComment);
     }
 
+    public TicketSystemConfig getConfig() {
+        return config;
+    }
 
+    public void setConfig(TicketSystemConfig config) {
+        this.config = config;
+    }
 }
