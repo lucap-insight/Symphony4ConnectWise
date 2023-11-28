ca1d848d67e7352dfc98101b3194dcfa02e9090e
lucap-insight <115674330+lucap-insight@users.noreply.github.com>
2023-11-28 13:30:24 +1100

    Created TicketServiceImpl class
    So far class only has comments

---
+++ src/main/java/com/insightsystems/symphony/tal/TicketServiceImpl.java
@@ -0,0 +1,20 @@
+package com.insightsystems.symphony.tal;
+
+import com.avispl.symphony.api.tal.dto.TicketSystemConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class TicketServiceImpl {
+    //* ----------------------------- VARIABLES ----------------------------- *//
+
+
+    //* ----------------------------- METHODS ----------------------------- *//
+
+
+    //* ----------------------------- HELPER METHODS ----------------------------- *//
+
+
+    //* ----------------------------- GETTERS / SETTERS ----------------------------- *//
+}
