2d704857702152cebcc0aa093504e715bb0e93f3
lucap-insight <115674330+lucap-insight@users.noreply.github.com>
2023-12-05 13:48:49 +1100

    Finalized getCWTicket and fixed validateUrl

--- src/main/java/com/insightsystems/symphony/tal/TicketServiceImpl.java
+++ src/main/java/com/insightsystems/symphony/tal/TicketServiceImpl.java
@@ -1,15 +1,12 @@
 package com.insightsystems.symphony.tal;
 
 import com.avispl.symphony.api.tal.dto.TicketSourceConfigProperty;
-import com.avispl.symphony.api.tal.dto.TicketSystemConfig;
 import com.avispl.symphony.api.tal.error.TalAdapterSyncException;
 import org.apache.commons.lang.NotImplementedException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
-import java.util.List;
 import java.util.Objects;
 
 public class TicketServiceImpl {
@@ -18,7 +15,7 @@ public class TicketServiceImpl {
     /**
      * Logger instance
      */
-    private static final Logger logger = LoggerFactory.getLogger(ConnectWiseTicket.class);
+    private static final Logger logger = LoggerFactory.getLogger(TicketServiceImpl.class);
 
     /**
     * Instance of ConnectWiseClient that is responsible for the communication with ConnectWise
@@ -37,33 +34,33 @@ public class TicketServiceImpl {
     /**
      * Retrieves the latest information on ConnectWise
      * @param CWTicket Ticket to be retrieved
-     * @return a new instance of ConnectWiseTicket with the latest information. Null if the ticket is not on CW.
-     * @throws TalAdapterSyncException
+     * @return a new instance of ConnectWiseTicket with the latest information. Null if the connection fails
+     * @throws TalAdapterSyncException if connection fails and has failed before for the same ticket
      */
-    public ConnectWiseTicket getNewestTicket(ConnectWiseTicket CWTicket) throws TalAdapterSyncException {
+    public ConnectWiseTicket getCWTicket(ConnectWiseTicket CWTicket) throws TalAdapterSyncException {
         ConnectWiseTicket refreshedCWTicket = null;
         // Attempt URL
         if (CWTicket.getUrl() != null) {
             try {
-                refreshedCWTicket = CWClient.GET(CWTicket.getUrl(), CWTicket);
+                refreshedCWTicket = CWClient.get(CWTicket.getUrl());
             } catch (TalAdapterSyncException e) {
-                logger.info("getNewestTicket: Ticket retrieval failed and it has failed before.");
-                return null;
+                if (Objects.equals(CWTicket.getExtraParams().get("connectionFailed"), "true"))
+                    throw e;
             }
         }
 
         // If URL did not work
-        if (CWTicket.getId() != null && refreshedCWTicket != null) {
-            try {
-                // Validation to make sure neither ID, URL, nor API Path is null
-                String url = validateUrl(CWClient.getConfig().getTicketSourceConfig().get(TicketSourceConfigProperty.URL),
-                        CWClient.getConfig().getTicketSourceConfig().get(TicketSourceConfigProperty.API_PATH),
-                        CWTicket.getId());
+        if (CWTicket.getId() != null && refreshedCWTicket == null) {
+            // Validation to make sure neither ID, URL, nor API Path is null
+            String url = validateUrl(CWClient.getConfig().getTicketSourceConfig().get(TicketSourceConfigProperty.URL),
+                    CWClient.getConfig().getTicketSourceConfig().get(TicketSourceConfigProperty.API_PATH),
+                    CWTicket.getId());
 
-                refreshedCWTicket = CWClient.GET(url, CWTicket);
+            try{
+                refreshedCWTicket = CWClient.get(url);
             } catch (TalAdapterSyncException e) {
-                logger.info("getNewestTicket: ticket retrieval failed");
-                return null;
+                if (Objects.equals(CWTicket.getExtraParams().get("connectionFailed"), "true"))
+                    throw e;
             }
         }
 
@@ -74,6 +71,12 @@ public class TicketServiceImpl {
             if (CWTicket.getExtraParams().putIfAbsent("connectionFailed", "true") != null) {
                 // "putIfAbsent" returns null if "put" worked, and returns the value found otherwise
                 CWTicket.getExtraParams().replace("connectionFailed", "true");
+            }
+        } else {
+            // Else the connection was successful
+            if (CWTicket.getExtraParams().putIfAbsent("connectionFailed", "false") != null) {
+                // "putIfAbsent" returns null if "put" worked, and returns the value found otherwise
+                CWTicket.getExtraParams().replace("connectionFailed", "false");
             }
         }
 
@@ -96,10 +99,11 @@ public class TicketServiceImpl {
 
     /**
      * Updates CW with the information in CWTicket
-     * @param CWTicket
+     * @param CWTicket Ticket with the latest information from Symphony
+     * @param refreshedTicket Ticket to be updated
      * @return
      */
-    public ConnectWiseTicket updateTicket(ConnectWiseTicket CWTicket) throws TalAdapterSyncException {
+    public ConnectWiseTicket updateTicket(ConnectWiseTicket CWTicket, ConnectWiseTicket refreshedTicket) throws TalAdapterSyncException {
         throw new NotImplementedException();
     }
 
@@ -108,6 +112,9 @@ public class TicketServiceImpl {
 
     /**
      * Throws a TalAdapterSyncException if the URL formed using the parameters contains any nulls
+     *
+     * @throws TalAdapterSyncException if any part of the url is null
+     * @return the formatted URL based on inputs
      */
     private String validateUrl(String url, String APIPath, String id) throws TalAdapterSyncException {
         if (url == null ||APIPath == null || id == null) {
@@ -118,7 +125,7 @@ public class TicketServiceImpl {
                     (APIPath == null? " API PATH":""),
                     HttpStatus.BAD_REQUEST);
         }
-        return url + APIPath + id;
+        return url + APIPath + "/" + id;
     }
 
     //* ----------------------------- GETTERS / SETTERS ----------------------------- *//
