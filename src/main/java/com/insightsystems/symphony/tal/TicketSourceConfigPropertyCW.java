aa362f15f2f4ef1e8038e48caef2ff3d71ea4483
lucap-insight <115674330+lucap-insight@users.noreply.github.com>
2024-04-04 11:53:00 +1100

    Minor changes

--- src/main/java/com/insightsystems/symphony/tal/TicketSourceConfigPropertyCW.java
+++ src/main/java/com/insightsystems/symphony/tal/TicketSourceConfigPropertyCW.java
@@ -4,10 +4,7 @@ import com.avispl.symphony.api.tal.dto.TicketSourceConfigProperty;
 
 public interface TicketSourceConfigPropertyCW extends TicketSourceConfigProperty {
     String CLIENTID = "clientId";
-
     String PUBLICKEY = "publicKey";
-
     String PRIVATEKEY = "privateKey";
-
     String COMPANYID = "companyId";
 }
