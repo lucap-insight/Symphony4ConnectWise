9df7ebf550def19d48d2c8f4fb96e56b1cc3852c
lucap-insight <115674330+lucap-insight@users.noreply.github.com>
2024-06-13 09:00:36 +1000

    Removed Login_Company_Id as a config property
    Plus minor comment edit for URL_Pattern_to_get_comments

--- src/main/java/com/insightsystems/symphony/tal/TicketSourceConfigPropertyCW.java
+++ src/main/java/com/insightsystems/symphony/tal/TicketSourceConfigPropertyCW.java
@@ -4,9 +4,8 @@ import com.avispl.symphony.api.tal.dto.TicketSourceConfigProperty;
 
 public interface TicketSourceConfigPropertyCW extends TicketSourceConfigProperty {
     String API_VERSION = "apiVersion"; // PUBLIC - ConnectWise API version, i.e. v2024.1
-    String LOGIN_COMPANY_ID = "loginCompanyId"; // PUBLIC - i.e. "connectwise"
     String URL_PATTERN_TO_GET_TICKET = "urlPatternToGetTicket"; // PUBLIC - REQUIRED - i.e. "/service/tickets"
-    String URL_PATTERN_TO_GET_COMMENTS = "urlPatternToGetComments"; // PUBLIC - REQUIRED - i.e. "/comments"
+    String URL_PATTERN_TO_GET_COMMENTS = "urlPatternToGetComments"; // PUBLIC - REQUIRED - i.e. "/notes"
     String CLIENT_ID = "clientId"; // SECRET - REQUIRED - Used in clientID header
     String COMPANY_ID = "companyId"; // PUBLIC - REQUIRED - Used in Authorization header (sometimes the same as loginCompanyId)
     String PUBLIC_KEY = "publicKey"; // SECRET - REQUIRED - Used in Authorization header
