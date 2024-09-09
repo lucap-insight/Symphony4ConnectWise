d13bc4e0c01b6d40a9f81a58c944c1dab4e4c8c0
lucap-insight <115674330+lucap-insight@users.noreply.github.com>
2024-09-10 08:19:49 +1000

    Created ConnectWiseConfiguration

---
+++ src/main/java/com/insightsystems/symphony/tal/ConnectWiseConfiguration.java
@@ -0,0 +1,29 @@
+package com.insightsystems.symphony.tal;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.avispl.symphony.api.tal.TalAdapter;
+import com.avispl.symphony.api.tal.TalConfigService;
+import com.avispl.symphony.api.tal.TalProxy;
+
+@Configuration
+public class ConnectWiseConfiguration {
+
+	/**
+	 * CW now adapter tal adapter will be scanned by TAL microservice and used for synchronization of tickets
+	 * TAL context will provide configuration and routing services
+	 *
+	 * @param talConfigService the tal config service
+	 * @param talRoutingService the tal routing service
+	 * @return the tal adapter
+	 */
+	@Bean
+	@ConditionalOnMissingBean
+	public TalAdapter talCWAdapter(
+			TalConfigService talConfigService,
+			TalProxy talRoutingService) {
+		return new TalAdapterImpl(talConfigService, talRoutingService);
+	}
+}
